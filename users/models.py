dd2f98c80605955350a7810eee452d2589f7415c
andywar65 <andy.war1965@gmail.com>
2022-02-11 20:22:07 +0100

    adding user and profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
+class User(AbstractUser):
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        if self.is_active:
+            p, created = Profile.objects.get_or_create(user_id = self.uuid)
+
+    class Meta:
+        ordering = ('username',)
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+        primary_key=True, editable=False )
+    avatar = models.ImageField(blank = True, null=True,
+        upload_to = 'uploads/users/')
+    bio = models.TextField(_("Short bio"), null=True, blank=True)
+    immutable = models.BooleanField(default = False,)
+
+    def get_full_name(self):
+        return self.user.get_full_name()
+    get_full_name.short_description = _('Name')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
