3b4cb55f8bc47a2d8ed9698c7ca0432bc3445797
andywar65 <andy.war1965@gmail.com>
2022-02-11 20:09:28 +0100

    adding users app

--- project/settings.py
+++ project/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     #third party
     'rest_framework',
+    #local
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,4 @@ MEDIA_URL = get_secret('MEDIA_URL') #'/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
