d449b5e5a74020b1d527c6f4ea7670886631b5b9
andywar65 <andy.war1965@gmail.com>
2022-02-12 14:19:16 +0100

    move function to profile

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import (User, Profile, )
 
 class UserAdmin(UserAdmin):
-    list_display = ('get_full_name', 'is_staff', 'is_active', 'is_superuser')
+    list_display = ('username', 'is_staff', 'is_active', 'is_superuser')
     list_editable = ('is_staff', 'is_active')
 
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -8,13 +8,6 @@ class User(AbstractUser):
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    def get_full_name(self):
-        if self.first_name and self.last_name:
-            return self.first_name + ' ' + self.last_name
-        else:
-            return self.username
-    get_full_name.short_description = _('Name')
-
     def save(self, *args, **kwargs):
         super(User, self).save(*args, **kwargs)
         if self.is_active:
@@ -33,11 +26,14 @@ class Profile(models.Model):
     immutable = models.BooleanField(default = False,)
 
     def get_full_name(self):
-        return self.user.get_full_name()
+        if self.user.first_name and self.user.last_name:
+            return self.user.first_name + ' ' + self.user.last_name
+        else:
+            return self.user.username
     get_full_name.short_description = _('Name')
 
     def __str__(self):
-        return self.user.get_full_name()
+        return self.get_full_name()
 
     class Meta:
         verbose_name = _('Profile')
