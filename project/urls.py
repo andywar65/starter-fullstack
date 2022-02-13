b59af63b9a3a1b93b930174cfe9da1b4886d10ec
andywar65 <andy.war1965@gmail.com>
2022-02-13 12:38:43 +0100

    static urlpattern

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
@@ -30,3 +32,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/v1/users/', include('users.api.v1.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
