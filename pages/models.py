22c3716e7b9619c1a91deeb73130d34007282b2b
andywar65 <andy.war1965@gmail.com>
2022-02-23 23:56:37 +0100

    working thumbnail saving

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,16 @@
 from PIL import Image
-from pathlib import Path
+from io import BytesIO
 
 from django.db import models
+from django.core.files import File
 from django.conf import settings
 from django.utils.translation import gettext as _
 from django.utils.timezone import now
+
+def modify_image_format(filename_ext):
+    conversion = {'JPG': 'JPEG',}
+    ext = filename_ext.split('.')[-1].upper()
+    return conversion[ext]
 
 class ImageData(models.Model):
 
@@ -22,16 +28,26 @@ class ImageData(models.Model):
     def save(self, *args, **kwargs):
         #make sure original image is loaded in db
         super(ImageData, self).save(*args, **kwargs)
+        #get file data
+        width = self.original.width
+        height = self.original.height
+        image = Image.open(self.original.path)
         filename_ext = self.original.name.replace('uploads/images/original/', '')
-        filename = filename_ext.split('.')[0]
+        #check title
         if not self.title:
-            self.title = filename
-        if not self.thumbnail == 'uploads/images/thumbnail/' + filename_ext:
-            self.original.open()
-            image = Image.open(self.original)
-            image.thumbnail((60,60), Image.ANTIALIAS)
-            path = Path(settings.MEDIA_ROOT).joinpath('uploads/images/thumbnail/').joinpath(filename_ext)
-            image.save( path, 'JPEG')
+            self.title = filename_ext.split('.')[0]
+        #check versions
+        if not self.thumbnail == self.original.name.replace('original', 'thumbnail'):
+            if width>height:
+                offset = (width-height)/2
+                thumb = image.crop((offset,0,width-offset,height))
+            elif height>width:
+                offset = (height-width)/2
+                thumb = image.crop((0,offset,width,height-offset))
+            thumb.thumbnail((60,60))
+            blob = BytesIO()
+            thumb.save(blob, format=modify_image_format(filename_ext))
+            self.thumbnail.save(filename_ext, File(blob), save=False)
         #save all the changes we've made
         super(ImageData, self).save(*args, **kwargs)
 
