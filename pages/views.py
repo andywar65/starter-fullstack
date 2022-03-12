16d810b86525626efef682c07c4a3f924a5d8e37
andywar65 <andy.war1965@gmail.com>
2022-03-12 20:36:39 +0100

    added home page view and template

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.views.generic import TemplateView
+from django.utils.crypto import get_random_string
+
+from .models import HomePage
 
 class HomePageTemplateView(TemplateView):
-    template_name = 'base_menu.html'
+    template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = HomePage.objects.first()
+        if not context['page']:
+            raise Http404(_("No Home Pages available"))
+        #we add this context to feed the standard gallery
+        context['main_gall_slug'] = get_random_string(7)
+        context['title'] = context['page'].title
+        #context for the page
+        context['images'] = context['page'].carousel.all()
+        return context
