d71e576c3a608a1740defbadeaaa8011c9dd3374
andywar65 <andy.war1965@gmail.com>
2022-03-24 23:58:50 +0100

    tested immutable user

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -16,10 +16,12 @@ class UserViewsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         print("\nTest user views")
-        boss = User.objects.create_superuser('boss', 'boss@example.com',
+        User.objects.create_superuser('boss', 'boss@example.com',
             'P4s5W0r6')
-        EmailAddress.objects.create(user_id=boss.uuid,
-            email=boss.email, verified=True, primary=True)
+        immutable = User.objects.create_user('immutable', 'immu@example.com',
+            'P4s5W0r6')
+        p = Permission.objects.get(codename='can_not_change_profile')
+        immutable.user_permissions.add(p)
 
     def test_user_views_status_code_302(self):
         print("\n-Test User Views not logged")
@@ -32,6 +34,19 @@ class UserViewsTest(TestCase):
         response = self.client.get(reverse('account_contact'))
         self.assertEqual(response.status_code, 302)
         print("\n--Test Account Contact redirect")
+
+    def test_immutable_user_views_status_code_403(self):
+        print("\n-Test Immutable User Views logged")
+        self.client.login(username='immutable', password='P4s5W0r6')
+        response = self.client.get(reverse('account_profile'))
+        self.assertEqual(response.status_code, 403)
+        print("\n--Test Immutable Account Profile forbidden")
+        response = self.client.get(reverse('account_delete'))
+        self.assertEqual(response.status_code, 403)
+        print("\n--Test Immutable Account Delete forbidden")
+        response = self.client.get(reverse('account_contact'))
+        self.assertEqual(response.status_code, 200)
+        print("\n--Test Immutable Account Contact success")
 
     def test_user_views_status_code_200(self):
         print("\n-Test User Views logged in")
