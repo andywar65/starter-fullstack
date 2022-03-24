477d30db044172ae243454a54993f02cb568a445
andywar65 <andy.war1965@gmail.com>
2022-03-24 23:44:26 +0100

    logged in tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -4,8 +4,7 @@ from django.conf import settings
 from django.test import TestCase, override_settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
-from django.contrib.auth.models import Permission, Group
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 from allauth.account.models import EmailAddress
 
@@ -17,21 +16,10 @@ class UserViewsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         print("\nTest user views")
-        admin = User.objects.create_user(username='admin', is_superuser=True,
-            password='P4s5W0r6', email='admin@example.com')
-        EmailAddress.objects.create(user_id=admin.uuid,
-            email='admin@example.com', verified=True, primary=True)
-        #viewer = User.objects.create_user(username='viewer',
-            #password='P4s5W0r6')
-        #adder = User.objects.create_user(username='adder',
-            #password='P4s5W0r6')
-        #permissions = Permission.objects.filter(
-            #codename__in=['view_building', 'view_plan',
-            #'view_photostation', 'view_stationimage'])
-        #for p in permissions:
-            #viewer.user_permissions.add(p)
-        #group = Group.objects.get(name='Building Manager')
-        #adder.groups.add(group)
+        boss = User.objects.create_superuser('boss', 'boss@example.com',
+            'P4s5W0r6')
+        EmailAddress.objects.create(user_id=boss.uuid,
+            email=boss.email, verified=True, primary=True)
 
     def test_user_views_status_code_302(self):
         print("\n-Test User Views not logged")
@@ -47,8 +35,7 @@ class UserViewsTest(TestCase):
 
     def test_user_views_status_code_200(self):
         print("\n-Test User Views logged in")
-        self.client.post(reverse('account_login'), {'username':'admin',
-            'password':'P4s5W0r6'})
+        self.client.login(username='boss', password='P4s5W0r6')
         response = self.client.get(reverse('account_profile'))
         self.assertEqual(response.status_code, 200)
         print("\n--Test Account Profile success")
