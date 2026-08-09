b6b6795bb1b133bad249768374f02b3dfe071802
andywar65 <andy.war1965@gmail.com>
2026-08-09 21:11:20 +0200

    use environs list (no whitespace in env file)

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.str("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 INTERNAL_IPS = [
     "127.0.0.1",
